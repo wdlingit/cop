1c6e178ea945e99c6655da6c6116ee0d2d50b2a6
wdlingit <wdlin@R418-20230609>
2025-03-11 14:55:28 +0800

    Enable group list input of GmtIntersectFisher.pl

--- scripts/GmtIntersectFisher.pl
+++ scripts/GmtIntersectFisher.pl
@@ -3,7 +3,27 @@
 use IPC::Open2;
 use Data::Dumper;
 
-$usage  = "Usage: GmtIntersect4fisher.pl <geneFile> <GMT> <bkgNumber> <rackjJARpath>\n";
+# for tmp file
+use strict;
+use warnings;
+use File::Temp;
+
+my $usage  = "Usage: GmtIntersect4fisher.pl <geneFile> <GMT> <bkgNumber> <rackjJARpath>\n";
+   $usage .= "     -group     : input <geneFile> is a two column group-gene file (default: NO)\n";
+
+my $inputIsGroupFile = 0;
+
+#Retrieve parameter
+my @arg_idx=(0..@ARGV-1);
+for my $i (0..@ARGV-1) {
+	if ($ARGV[$i] eq '-group') {
+		$inputIsGroupFile = 1;
+		delete $arg_idx[$i];
+    }
+}
+my @new_arg;
+for (@arg_idx) { push(@new_arg,$ARGV[$_]) if (defined($_)); }
+@ARGV=@new_arg;
 
 my $geneFile = shift or die $usage;
 my $gmtFile  = shift or die $usage;
@@ -11,18 +31,6 @@ my $bkgNum   = shift or die $usage;
 my $jarPath  = shift or die $usage;
 
 my $pid = open2( *queryOUT, *queryIN,"java -classpath $jarPath statistics.FisherExactTest");
-
-# read gene file
-my %geneHash = ();
-
-open(FILE,"<$geneFile");
-while(<FILE>){
-    next if /^#/;
-    chomp;
-    my @t=split;
-    $geneHash{$t[0]}=1;
-}
-close FILE;
 
 # read GMT file
 my %setName = ();
@@ -43,29 +51,74 @@ while(<FILE>){
 }
 close FILE;
 
-# for each set, compute four numbers for fisher exact test
-for my $id (sort keys %setGene){
+if($inputIsGroupFile){
+    my %groupGeneHash = ();
 
-    my $oTarget=0;
-    for my $x (keys %geneHash){
-        $oTarget++ if exists $setGene{$id}{$x};
+    open(FILE,"<$geneFile");
+    while(<FILE>){
+        chomp;
+        
+        my @t=split;
+        $groupGeneHash{$t[0]}{$t[1]}=1;
     }
+    close FILE;
     
-    my $setNum = keys %{$setGene{$id}};
-    my $oBackground = $setNum-$oTarget;
-
-    my $targetNum = keys %geneHash;
-    my $xTarget = $targetNum - $oTarget;
-
-    my $xBackground = $bkgNum - $oTarget - $oBackground - $xTarget;
-    
-    if($oTarget>0){
-        print "$id\t$setName{$id}\t$oTarget\t$oBackground\t$xTarget\t$xBackground\t";
-
-        print queryIN "$oTarget\t$oBackground\t$xTarget\t$xBackground\n";
-        my $msg = <queryOUT>;
-        $msg =~ s/^\s+|\s+$//g;
-        my @t=split(/\s+/,$msg);
-        print join("\t",@t)."\n";
+    for my $group (keys %groupGeneHash){
+        my $tempfile = File::Temp->new();
+        for my $g (keys %{$groupGeneHash{$group}}){
+            print $tempfile "$g\n";
+        }
+        close $tempfile;
+        
+        gmtComputeOneSet($tempfile->filename,$group);
     }
+}else{
+    gmtComputeOneSet($geneFile,"");
+}
+
+### subroutine
+sub gmtComputeOneSet {
+    my $geneSetFile = shift;
+    my $groupName   = shift;
+
+    # read gene file
+    my %geneHash = ();
+    
+    open(FILE,"<$geneSetFile");
+    while(<FILE>){
+        next if /^#/;
+        chomp;
+        my @t=split;
+        $geneHash{$t[0]}=1;
+    }
+    close FILE;
+
+    # for each set, compute four numbers for fisher exact test
+    for my $id (sort keys %setGene){
+    
+        my $oTarget=0;
+        for my $x (keys %geneHash){
+            $oTarget++ if exists $setGene{$id}{$x};
+        }
+        
+        my $setNum = keys %{$setGene{$id}};
+        my $oBackground = $setNum-$oTarget;
+        
+        my $targetNum = keys %geneHash;
+        my $xTarget = $targetNum - $oTarget;
+        
+        my $xBackground = $bkgNum - $oTarget - $oBackground - $xTarget;
+        
+        if($oTarget>0){
+            print "$groupName\t" if length($groupName)>0;
+            print "$id\t$setName{$id}\t$oTarget\t$oBackground\t$xTarget\t$xBackground\t";
+            
+            print queryIN "$oTarget\t$oBackground\t$xTarget\t$xBackground\n";
+            my $msg = <queryOUT>;
+            $msg =~ s/^\s+|\s+$//g;
+            my @t=split(/\s+/,$msg);
+            print join("\t",@t)."\n";
+        }
+    }
+
 }
